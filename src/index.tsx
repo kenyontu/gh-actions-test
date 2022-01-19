8e6143ad7e1152944e0e108516878e3f45fddb71
Kenyon Tu <kenyontu@outlook.com>
2022-01-19 14:43:47 +0800

    feat: add routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './index.css'
 import App from './App'
+import About from './About'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/gh-actions-test">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 )
